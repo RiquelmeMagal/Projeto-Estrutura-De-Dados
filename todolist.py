83b385f4a5ad8476c6a41f4b5a0dd564d93313b4
RiquelmeMagal <riquelme.souza@arapiraca.ufal.br>
2023-03-04 21:18:11 -0300

    Update

--- todolist.py
+++ todolist.py
@@ -1,15 +1,16 @@
 class Node:
-    def __init__(self, tarefa, concluida = False):
+    def __init__(self, tarefa, concluida=False):
         self.tarefa = tarefa
         self.concluida = concluida
         self.proximo = None
+
 
 class ListaTarefas:
     def __init__(self):
         self.primeiro = None
         self.tamanho = 0
 
-    def adicionar_tarefas(self,tarefa):
+    def adicionar_tarefa(self, tarefa):
         novo_node = Node(tarefa)
         if self.primeiro is None:
             self.primeiro = novo_node
@@ -17,7 +18,7 @@ class ListaTarefas:
             atual = self.primeiro
             while atual.proximo is not None:
                 atual = atual.proximo
-            atual = novo_node
+            atual.proximo = novo_node
 
     def remover_tarefa(self, tarefa):
         if self.primeiro is None:
@@ -25,27 +26,37 @@ class ListaTarefas:
         if self.primeiro.tarefa == tarefa:
             self.primeiro = self.primeiro.proximo
             return
-    
+
         atual = self.primeiro
         while atual.proximo is not None:
-            atual.proximo = atual.proximo.proximo
-            return
-        atual = atual.proximo
-    
-    def marcar_como_concluida(self,tarefa):
+            if atual.proximo.tarefa == tarefa:
+                atual.proximo = atual.proximo.proximo
+                return
+            atual = atual.proximo
+
+    def marcar_como_concluida(self, tarefa):
         atual = self.primeiro
         while atual is not None:
             if atual.tarefa == tarefa:
                 atual.concluida = True
                 return
             atual = atual.proximo
-        
+
     def marcar_como_nao_concluida(self, tarefa):
         atual = self.primeiro
         while atual is not None:
-            status = 'Concluida.' if atual.concluida else 'Não concluída.'
+            if atual.tarefa == tarefa:
+                atual.concluida = False
+                return
+            atual = atual.proximo
+
+    def listar_tarefas(self):
+        atual = self.primeiro
+        while atual is not None:
+            status = 'Concluída' if atual.concluida else 'Não concluída'
             print(f'{atual.tarefa} ({status})')
             atual = atual.proximo
+
 
 lista = ListaTarefas()
 
@@ -53,8 +64,8 @@ lista.adicionar_tarefa("Fazer compras")
 lista.adicionar_tarefa("Lavar roupa")
 lista.adicionar_tarefa("Estudar Python")
 
-lista.listar_tarefa()
-# Saída: 
+lista.listar_tarefas()
+# Saída:
 # Fazer compras (Não Concluída)
 # Lavar roupa (Não Concluída)
 # Estudar Python (Não Concluída)
